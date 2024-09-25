d37f6e06f0294c8ac42a69ae117a4e3271f4eece
MUKILTEO\1721634 <1721634@mukilteo.wednet.edu>
2024-09-25 09:54:43 -0700

    New enemy sprites Simon whip animation

--- Retro Remake/Assets/Sprites/Movement.cs
+++ Retro Remake/Assets/Sprites/Movement.cs
@@ -12,14 +12,20 @@ public class Movement : MonoBehaviour
     public float speed;
     public float speedUp;
     public bool facingRight = false;
+    private bool isJumping;
+    private bool isGrounded;
+    public LayerMask groundLayer;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.BoxCast(transform.position + Vector3.down, new Vector2(1, 0.1f), 0, Vector2.one, 0, groundLayer);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isFalling", isGrounded == false && GetComponent<Rigidbody2D>().velocity.y < -2);
         if (Input.GetKey(left))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, GetComponent<Rigidbody2D>().velocity.y);
-            anim.SetFloat("horizontal", speed);
+            anim.SetBool("isMoving", true);
             if (facingRight == true)
             {
                 Flip();
@@ -27,12 +33,12 @@ public class Movement : MonoBehaviour
         }
         if (Input.GetKeyUp(left))
         {
-            anim.SetFloat("horizontal", 0);
+            anim.SetBool("isMoving", false);
         }
         if (Input.GetKey(right))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
-            anim.SetFloat("horizontal", speed);
+            anim.SetBool("isMoving", true);
             if (facingRight == false)
             {
                 Flip();
@@ -40,11 +46,18 @@ public class Movement : MonoBehaviour
         }
         if (Input.GetKeyUp(right))
         {
-            anim.SetFloat("horizontal", 0);
+            anim.SetBool("isMoving", false);
         }
         if (Input.GetKeyDown(up))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, speedUp);
+            anim.SetBool("isJumping", true);
+            isJumping = true;
+        }
+        if (isJumping = true && GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            anim.SetBool("isJumping", false);
+            isJumping = false;
         }
     }
     void Flip()
@@ -52,4 +65,22 @@ public class Movement : MonoBehaviour
         facingRight = !facingRight;
         transform.Rotate(0, 180, 0);
     }
+    //void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == ("Ground"))
+    //    {
+    //        isGrounded = true;
+    //        anim.SetBool("isGrounded", true);
+    //        anim.SetBool("isFalling", false);
+    //    }
+    //}
+
+    //void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == ("Ground"))
+    //    {
+    //        isGrounded = false;
+    //        anim.SetBool("isGrounded", false);
+    //    }
+    //}
 }
